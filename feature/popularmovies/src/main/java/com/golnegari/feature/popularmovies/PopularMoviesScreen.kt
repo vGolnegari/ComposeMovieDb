f228486e4e437d94dc52b4398cb94c2770053dfd
Vahid Golnegari <v.golnegari@gmail.com>
2023-10-28 13:48:02 +0330

    TopAppBar is added to PopularMoviesScreen

--- feature/popularmovies/src/main/java/com/golnegari/feature/popularmovies/PopularMoviesScreen.kt
+++ feature/popularmovies/src/main/java/com/golnegari/feature/popularmovies/PopularMoviesScreen.kt
@@ -1,6 +1,7 @@
 package com.golnegari.feature.popularmovies
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -14,17 +15,22 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -49,18 +55,31 @@ fun PopularMoviesScreen(
         uiStateState = viewModel.uiStateFlow.collectAsState(),
         singleEventFlow = viewModel.singleEventFlow,
         sendAction = viewModel::sendAction
-    ) {data, snackBarHostState ->
-        Scaffold(snackbarHost = { SnackbarHost(hostState = snackBarHostState) }) { paddingValues ->
-            LazyVerticalStaggeredGrid(modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(8.dp),
+    ) { data, snackBarHostState ->
+        Scaffold(snackbarHost = { SnackbarHost(hostState = snackBarHostState) }, topBar = {
+            TopAppBar(title = { Text(text = "Popular", color = Color.White) }, actions = {
+                IconButton(onClick = { }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_arrangement),
+                        contentDescription = "arrangement",
+                        colorFilter = ColorFilter.tint(
+                            Color.White
+                        )
+                    )
+                }
+            }, colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.DarkGray))
+        }) { paddingValues ->
+            LazyVerticalStaggeredGrid(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(8.dp),
                 columns = StaggeredGridCells.Fixed(count = 3),
                 verticalItemSpacing = 4.dp,
                 horizontalArrangement = Arrangement.spacedBy(4.dp),
-                ){
+            ) {
                 items(data?.movies ?: emptyList()) { item ->
-                    MovieItemContent(info =item , onMovieCLicked = {id ->
+                    MovieItemContent(info = item, onMovieCLicked = { id ->
                         viewModel.sendAction(action = PopularMoviesAction.OnGotoMovieDetail(movieId = id))
                     })
                 }
@@ -70,16 +89,37 @@ fun PopularMoviesScreen(
 }
 
 @Composable
-private fun MovieItemContent(modifier: Modifier = Modifier,info : Movie,onMovieCLicked : (movieId : Int) -> Unit) {
-    Column(modifier = modifier
-        .height(180.dp)
-        .background(color = Color.DarkGray)
-        .clickable {
-            onMovieCLicked.invoke(info.id)
-        }, verticalArrangement = Arrangement.spacedBy(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        AsyncImage(model = info.posterPath, contentDescription = "poster", modifier = Modifier.height(140.dp), contentScale = ContentScale.Crop)
-        Text(modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp),text = info.title, fontSize = 14.sp,color = Color.White, overflow = TextOverflow.Ellipsis, maxLines = 2, textAlign = TextAlign.Center)
+private fun MovieItemContent(
+    modifier: Modifier = Modifier,
+    info: Movie,
+    onMovieCLicked: (movieId: Int) -> Unit
+) {
+    Column(
+        modifier = modifier
+            .height(180.dp)
+            .background(color = Color.DarkGray)
+            .clickable {
+                onMovieCLicked.invoke(info.id)
+            },
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(
+            model = info.posterPath,
+            contentDescription = "poster",
+            modifier = Modifier.height(140.dp),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+            text = info.title,
+            fontSize = 14.sp,
+            color = Color.White,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 2,
+            textAlign = TextAlign.Center
+        )
     }
 }
